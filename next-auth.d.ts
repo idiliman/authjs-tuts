854b13a1276f48d40a25d8aa373fdd56426a402e
idil <idil.iman1999@gmail.com>
2024-01-04 11:04:50 +0800

    feat: settings page

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,7 +1,9 @@
 import NextAuth, { type DefaultSession } from 'next-auth';
 
 export type ExtendedUser = DefaultSession['user'] & {
-  role: 'ADMIN' | 'USER';
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
 };
 
 declare module 'next-auth' {
